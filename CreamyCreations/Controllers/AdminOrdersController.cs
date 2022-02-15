74f59867b72a8c4636450b298daa57875a769ca9
Alex Lo <7253alex@gmail.com>
2022-02-15 12:34:55 -0800

    first draft of admin orders page

---
+++ CreamyCreations/Controllers/AdminOrdersController.cs
@@ -0,0 +1,29 @@
+﻿using CreamyCreations.Data;
+using CreamyCreations.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreamyCreations.Controllers
+{
+    public class AdminOrdersController : Controller
+    {
+        private readonly ILogger<AdminOrdersController> _logger;
+        private readonly ApplicationDbContext _context;
+        public AdminOrdersController(ILogger<AdminOrdersController> logger,
+                              ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            AdminOrdersRepo adminOrders = new AdminOrdersRepo(_context);
+            var orders = adminOrders.All();
+            return View(orders);
+        }
+    }
+}
---
+++ CreamyCreations/Repositories/AdminOrdersRepo.cs
@@ -0,0 +1,33 @@
+﻿using CreamyCreations.Data;
+using CreamyCreations.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreamyCreations.Repositories
+{
+    public class AdminOrdersRepo
+    {
+        ApplicationDbContext _context;
+
+        public AdminOrdersRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // Get all customer orders in the database (viewable to only the admin)
+        public IEnumerable<AdminOrdersVM> All()
+        {
+            var adminOrders = _context.Orders.Select(u => new AdminOrdersVM()
+            {
+                orderID = u.OrderId,
+                weddingCakeID = u.WeddingCakeId,
+                userID = u.UserId,
+                deliveryDate = u.DeliveryDate
+
+            });
+            return adminOrders;
+        }
+    }
+}
