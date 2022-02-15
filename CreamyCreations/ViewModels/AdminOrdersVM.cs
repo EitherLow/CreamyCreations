3daeeefb04d710fd2bd6891c15c2dbba9d32ecb1
Alex Lo <7253alex@gmail.com>
2022-02-15 13:52:46 -0800

    created admin controller so url will be admin/orders

--- CreamyCreations/ViewModels/AdminOrdersVM.cs
+++ CreamyCreations/ViewModels/AdminOrdersVM.cs
@@ -8,11 +8,12 @@ namespace CreamyCreations.ViewModels
 {
     public class AdminOrdersVM
     {
+
         [Key]
+        public int Id { get; set; }
+
         [Display(Name = "Order ID")]
         public int orderID { get; set; }
-
-        public int addressID { get; set; }
 
         public int weddingCakeID { get; set; }
 
