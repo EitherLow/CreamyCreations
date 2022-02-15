ad64550cf487f7f97b706baca88e5c696926df53
Alex Lo <7253alex@gmail.com>
2022-02-15 12:34:55 -0800

    first draft of admin orders page

---
+++ CreamyCreations/ViewModels/AdminOrdersVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreamyCreations.ViewModels
+{
+    public class AdminOrdersVM
+    {
+        [Key]
+        [Display(Name = "Order ID")]
+        public int orderID { get; set; }
+
+        public int addressID { get; set; }
+
+        public int weddingCakeID { get; set; }
+
+        public int userID { get; set; }
+
+        public DateTime deliveryDate { get; set; }
+    }
+}
