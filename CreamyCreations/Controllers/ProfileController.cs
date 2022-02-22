675b40de56c011e0aa72b2d9de9e5e5c2ba8a5d7
Billy Bui <billybui768@gmail.com>
2022-02-22 11:42:21 -0800

    Added ability to edit user profile

--- CreamyCreations/Controllers/ProfileController.cs
+++ CreamyCreations/Controllers/ProfileController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CreamyCreations.Data;
+using CreamyCreations.Models;
+using CreamyCreations.Repositories;
 
 namespace CreamyCreations.Controllers
 {
@@ -31,6 +33,41 @@ namespace CreamyCreations.Controllers
             Response.Redirect("/");
 
             return new EmptyResult();
+        }
+
+        public IActionResult Edit()
+        {
+            if (User.Identity != null && User.Identity.Name != null)
+            {
+                string getUserName = User.Identity.Name;
+                var userData = _context.Users.FirstOrDefault(user => user.Email == getUserName);
+                return View(userData);
+            }
+            
+            Response.Redirect("/");
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id,User user)
+        {
+            Console.WriteLine("ProfileController: Line 54 -> {0}",id);
+            if (ModelState.IsValid)
+            {
+                var repo = new ProfileRepo(_context);
+                var userEmail = User.Identity.Name;
+                var status = repo.UpdateProfile(user, id);
+                if (!status)
+                {
+                    ViewBag.Error = "Something went wrong. Try again later";
+                }
+                // goto the profile page if we the account update is successful
+                ViewBag.statusMessage = "Successfully Updated your profile";
+                return View("Index", user);
+            }
+
+            ViewBag.statusMessage = "Something went wrong!";
+            return View();
         }
     }
 }
