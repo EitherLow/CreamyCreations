cb6d27bce353e99da493d26f60b20a2d9b63661a
Billy Bui <billybui768@gmail.com>
2022-02-22 10:24:32 -0800

    Added user details page

---
+++ CreamyCreations/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CreamyCreations.Data;
+
+namespace CreamyCreations.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            if (User.Identity != null)
+            {
+                string getUserName = User.Identity.Name;
+                Console.WriteLine("Console Write: Profile Controller line 23 -> {0}",getUserName);
+                // longer way
+                /*var userData = _context.Users.Where(user => user.Email == getUserName).FirstOrDefault();*/
+                var userData = _context.Users.FirstOrDefault(user => user.Email == getUserName);
+                return View(userData);
+            }
+            // TODO: Please fix this so the page does not crash???
+            return null;
+        }
+    }
+}
