4d5e12564ddac1254249bf6d5dfbe067d9df1a3a
Billy Bui <billybui768@gmail.com>
2022-03-01 11:35:20 -0800

    Updated vm with new key

--- CreamyCreations/ViewModels/UserProfileVM.cs
+++ CreamyCreations/ViewModels/UserProfileVM.cs
@@ -9,11 +9,14 @@ namespace CreamyCreations.ViewModels
     public class UserProfileVM
     {
         [Key]
+        public int UserId { get; set; }
+
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
