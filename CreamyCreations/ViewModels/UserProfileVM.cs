40558906997d978dead49cbc596e8d31fcb442fb
Billy Bui <billybui768@gmail.com>
2022-03-01 11:29:05 -0800

    Added view model

---
+++ CreamyCreations/ViewModels/UserProfileVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreamyCreations.ViewModels
+{
+    public class UserProfileVM
+    {
+        [Key]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
