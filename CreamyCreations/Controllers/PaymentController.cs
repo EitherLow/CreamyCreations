29b48fa56fe365593a8e69b27b4f0d6404cad2c3
Billy Bui <billybui768@gmail.com>
2022-03-16 11:21:48 -0700

    Added display details for payments

---
+++ CreamyCreations/Controllers/PaymentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CreamyCreations.Data;
+using CreamyCreations.Repositories;
+
+namespace CreamyCreations.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private PaymentRepo paymentRepo;
+        private readonly ApplicationDbContext _context;
+
+        public PaymentController(ApplicationDbContext context)
+        {
+            _context = context;
+            paymentRepo = new PaymentRepo(context);
+        }
+
+        // assume that the wedding cake id is passed in.
+        public IActionResult Index(int? id = 0)
+        {
+            // Only send the request if we actually do have and id
+            //Assume that the user may go back to the payment page without going
+            // through the creation page. Redirect if the id is not set
+            if (id != null)
+            {
+               var thing =  paymentRepo.GetCakeDetails(id.Value);
+               return View(thing);
+
+            }
+            // redirect the user if the user directly requested the payment page
+            Response.Redirect("/");
+            return new EmptyResult();
+        }
+    }
+}
---
+++ CreamyCreations/ViewModels/PaymentVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CreamyCreations.Models;
+
+namespace CreamyCreations.ViewModels
+{
+    public class PaymentVM
+    {
+        [Key]
+        public int WeddingCakeId { get; set; }
+        public List<Filling> Fillings { get; set; }
+        public List<Cover>  Covers { get; set; }
+        public List<Decoration> Decorations { get; set; }
+    }
+}
