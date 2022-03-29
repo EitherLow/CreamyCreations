1b7559574e13b95cacd4105e98607f334e91705e
Alex Lo <7253alex@gmail.com>
2022-03-29 09:44:46 -0700

    test push

---
+++ CreamyCreations/Repositories/AdminCustomizationRepo.cs
@@ -0,0 +1,18 @@
+﻿using CreamyCreations.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreamyCreations.Repositories
+{
+    public class AdminCustomizationRepo
+    {
+        ApplicationDbContext _context;
+        public AdminCustomizationRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+    }
+}
