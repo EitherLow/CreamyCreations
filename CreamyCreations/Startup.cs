120dd0726470fe887dd94c15b18a8dc7435f8492
Billy Bui <billybui768@gmail.com>
2022-03-29 15:11:03 -0700

    Add start up changes

--- CreamyCreations/Startup.cs
+++ CreamyCreations/Startup.cs
@@ -33,7 +33,7 @@ namespace CreamyCreations
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
